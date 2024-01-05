791978d4015fcf14aa2a8da3694fdf234a4e3b96
HakkimFr <100283675+HakkimFr@users.noreply.github.com>
2024-01-05 22:14:42 +0400

    Update Index.java
    test

--- Index.java
+++ Index.java
@@ -5,7 +5,7 @@ class Index {
     int first = 100;
     int second = 200;
 
-    // add two numbers
+    // add two numbers test
     int sum = first + second;
     System.out.println(first + " + " + second + " hi to all = "  + sum);
   }
