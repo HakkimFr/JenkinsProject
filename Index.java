1213178184fb7612421e9f255ca4e483554f7f05
HakkimFr <100283675+HakkimFr@users.noreply.github.com>
2024-01-05 21:27:35 +0400

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,12 @@
+class Index {
+
+  public static void main(String[] args) {
+    
+    int first = 100;
+    int second = 200;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " hi to all = "  + sum);
+  }
+}
