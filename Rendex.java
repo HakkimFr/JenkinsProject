2588b69c1fb55de4283348c8d8cfba4794dc3610
HakkimFr <100283675+HakkimFr@users.noreply.github.com>
2024-01-06 00:00:54 +0400

    Update Rendex.java

--- Rendex.java
+++ Rendex.java
@@ -5,7 +5,7 @@ class Rendex {
     int first = 100;
     int second = 200;
 
-    // add two numbers test d test test  ssss s  dsss
+    // add two numbers test d test test  ssss s  dsssd
     int sum = first + second;s
     System.out.println(first + " + " + second + " hi to all = "  + sum);
   }
